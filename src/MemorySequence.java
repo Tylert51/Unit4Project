e528c4c870af7de47d8209c38ef030258c0aaff8
tyler <ttang7910@bths.edu>
2022-11-10 12:32:30 -0500

    added some box methods

--- src/MemorySequence.java
+++ src/MemorySequence.java
@@ -1,24 +1,53 @@
 public class MemorySequence {
-   private final String ONE = "01"; private String oneValue;
-   private final String TWO = "02"; private String twoValue;
-   private final String THREE = "03"; private String threeValue;
-   private final String FOUR = "04"; private String fourValue;
-   private final String FIVE = "05"; private String fiveValue;
-   private final String SIX = "06"; private String sixValue;
-   private final String SEVEN = "07"; private String sevenValue;
-   private final String EIGHT = "08"; private String eightValue;
-   private final String NINE = "09"; private String nineValue;
-   private final String TEN = "10"; private String tenValue;
-   private final String ELVEN = "11"; private String elevenValue;
-   private final String TWELVE = "12"; private String twelveValue;
-   private final String THIRTEEN = "13"; private String thirteenValue;
-   private final String FOURTEEN = "14"; private String fourteenValue;
-   private final String FIFTEEN = "15"; private String fifteenValue;
-   private final String SIXTEEN = "16"; private String sixteenValue;
-   private final String EMPTY = "    ";
 
+   private String[][] boxNumbers;
+   private String[][] blank;
+   private final String DIV_LINE = "|----|----|----|----|";
 
-   public MemorySequence (String one, String two, String three, String four, String five, String six, String seven, String eight, String nine, String ten, String eleven, String twelve, String thirteen, String fourteen, String fifteen, String sixteen)
+   public MemorySequence() {
+      String[][] boxNumbers = new String[4][4];
+      int count = 1;
+      for (int row = 0; row < boxNumbers.length; row++) {
+         for (int column = 0; column < boxNumbers[0].length; column++) {
+            if (count < 10) {
+               boxNumbers[row][column] = "0" + count;
+            } else {
+               boxNumbers[row][column] = "" + count;
+            }
+
+            count++;
+         }
+      }
+      this.boxNumbers = boxNumbers;
+
+      String[][] blank = new String[4][4];
+      for (int row = 0; row < blank.length; row++) {
+         for (int column = 0; column < blank[0].length; column++) {
+            blank[row][column] = "  ";
+         }
+      }
+      this.blank = blank;
+   }
+
+   private String printArrayBox (String[][] array) {
+      String box = DIV_LINE + "\n";
+
+      for (int row = 0; row < array.length; row++) {
+         for (int column = 0; column < array[0].length; column++) {
+            box += "| " + array[row][column] + " ";
+         }
+         box += "|\n" + DIV_LINE + "\n";
+      }
+      return box;
+   }
+
+   public String printBox() {
+      return printArrayBox(boxNumbers);
+   }
+
+   public String emptyBox() {
+      return printArrayBox(blank);
+   }
 
 
 }
--- src/MemorySequenceRunner.java
+++ src/MemorySequenceRunner.java
@@ -1,5 +1,7 @@
 public class MemorySequenceRunner {
     public static void main(String[] args) {
-
+        MemorySequence game = new MemorySequence();
+        System.out.print(game.emptyBox());
+        System.out.print(game.printBox());
     }
 }
