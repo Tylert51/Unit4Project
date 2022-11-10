f98ce7df4bf1fa467083bfd8a67b2f6aa1abea0d
tyler <ttang7910@bths.edu>
2022-11-09 22:59:18 -0500

    created instance variables

---
+++ src/MemorySequence.java
@@ -0,0 +1,24 @@
+public class MemorySequence {
+   private final String ONE = "01"; private String oneValue;
+   private final String TWO = "02"; private String twoValue;
+   private final String THREE = "03"; private String threeValue;
+   private final String FOUR = "04"; private String fourValue;
+   private final String FIVE = "05"; private String fiveValue;
+   private final String SIX = "06"; private String sixValue;
+   private final String SEVEN = "07"; private String sevenValue;
+   private final String EIGHT = "08"; private String eightValue;
+   private final String NINE = "09"; private String nineValue;
+   private final String TEN = "10"; private String tenValue;
+   private final String ELVEN = "11"; private String elevenValue;
+   private final String TWELVE = "12"; private String twelveValue;
+   private final String THIRTEEN = "13"; private String thirteenValue;
+   private final String FOURTEEN = "14"; private String fourteenValue;
+   private final String FIFTEEN = "15"; private String fifteenValue;
+   private final String SIXTEEN = "16"; private String sixteenValue;
+   private final String EMPTY = "    ";
+
+
+   public MemorySequence (String one, String two, String three, String four, String five, String six, String seven, String eight, String nine, String ten, String eleven, String twelve, String thirteen, String fourteen, String fifteen, String sixteen)
+
+
+}
