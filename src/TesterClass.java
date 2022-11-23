a9afcc135cd2348460f26edcec34a09960d6e4a3
tyler <ttang7910@bths.edu>
2022-11-23 00:57:00 -0500

    added TesterClass

---
+++ src/TesterClass.java
@@ -0,0 +1,7 @@
+public class TesterClass {
+    public static void main(String[] args) {
+        MemorySequence test = new MemorySequence();
+
+        System.out.println(test.lightBox());
+    }
+}
